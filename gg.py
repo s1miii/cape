ca5790fc34c72747090287e1c7d1616fcacec83e
Simi <84782994+b3lalangg@users.noreply.github.com>
2021-08-24 00:05:23 +0700

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,116 +1,52 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# -- import Modules --
-import os
-import sys
-import json
-import random
 import requests
-import platform
-import threading
-
-py_version = platform.python_version()
-if int(py_version[0]) == 2:
-   from Queue import Queue
-if int(py_version[0]) == 3:
-   from queue import Queue
-
-# -- Colors --
-if sys.platform in ["linux","linux2"]:
-   purple = '\033[95m'
-   blue = '\033[94m'
-   cyan = '\033[96m'
-   green = '\033[92m'
-   yellow = '\033[93m'
-   red = '\033[91m'
-   end = '\033[0m'
-   bold = '\033[1m'
-   u = '\033[4m'
-else:
-   purple = ''
-   blue = ''
-   cyan = ''
-   green = ''
-   yellow = ''
-   red = ''
-   end = ''
-   bold = ''
-   u = ''
-
-# -- Clear --
-if sys.platform == 'win32':
-   os.system('cls')
-else:
-   os.system('clear')
-
-# -- Class --
-class Reverse_Ip:
-   def __init__(self):
-      self.url = 'https://api.viewdns.info/reverseip/?host="+ip+"&apikey=e0cbb9dd32ea828d23759ffbc735f2c5f4541655'
-      self.banner()
-      self.save = 'result.txt'
-   def reverse(self, domain):
-      req = requests.get(self.url+domain)
-      if req.status_code == 200:
-         jumlah = len(req.json()['result'])
-         if jumlah > 10  and  jumlah < 100:
-            color = yellow
-         elif jumlah < 10:
-            color = red
-         else:
-             color = green
-         print ("{} >> ({} domains)".format(color+domain, str(jumlah)))
-         res = req.json()
-         if res['result']:
-            domains = '\n'.join(res['result'])
-            open(self.save, 'a').write(domains+'\n')
-   def start(self, q):
-     while not q.empty():
-       dom = q.get()
-       if dom:
-          self.reverse(dom)
-       q.task_done()
-   def banner(self):
-      b = '''
-     ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤
-   ◢◤                            ◢◤
-  ◢◤  Reverse Ip (Unlimited)    ◢◤
- ◢◤  By Zeerx7 - XpoitSec-ID   ◢◤
-◢◤                            ◢◤
- ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤ ◢◤
-'''   
-      co = random.choice([red,purple,blue])
-      ct = random.choice([green,cyan,yellow])
-      banner = b.replace('◢◤',co+'◢◤').replace('R',ct+'R').replace('B',ct+'B')
-      print(banner)
-   def input(self, q):
-      #input based on python version
-      if int(py_version[0]) == 3:
-           return input(str(q))
-      elif int(py_version[0]) == 2:
-           return raw_input(str(q))
-   def main(self):
-      jobs = Queue()
-      thread = 10
-      try:
-          filename = self.input(yellow+'Domains/Ip List -> '+green)
-          list = open(filename, 'r').read().splitlines()
-      except IOError:
-           exit(red+"File Not Found!\n")
-      except FileNotFoundError:
-           exit(red+"File Not Found!\n")
-      print (red+'\nI suggest using no more than (10) threads \nor the tools will crash!')
-      thread = int(self.input(yellow+'Thread -> '+green))
-      print(blue+'\nResult will be saved in: result.txt')
-      for dom in list:
-         if dom:
-             jobs.put(dom)
-
-      for x in range(thread):
-         worker = threading.Thread(target=self.start, args=(jobs,))
-         worker.start()
-
-# -- Main --
-if __name__ == '__main__':
-     Reverse_Ip().main()
+import json
+import argparse
+import socket
+def args():
+    arg=argparse.ArgumentParser()
+    arg.add_argument('-i','--ip',dest='ip',help='-i or --ip Argument Pass IP')
+    arg.add_argument('-l','--list',dest='list',help='-l or --list Argument Pass List IP')
+    return [arg.parse_args().ip,arg.parse_args().list]
+def extract(ip):
+    save = open('Sites.txt','a')
+    try:
+        i=socket.gethostbyaddr(str(ip))
+        save.write(i[0]+' | '+str(ip)+'\n')
+    except:
+        req=requests.get('https://api.viewdns.info/reverseip/?host='+str(ip)+'&apikey=e0cbb9dd32ea828d23759ffbc735f2c5f4541655&output=json').text
+        js=json.loads(req)
+        count=int(js['response']['domain_count'])
+        for i in range(0,count):
+            save.write(js['response']['domains'][i]['name']+' | '+str(ip)+'\n')
+print('''
+       ////////////////////////////////////////
+     ///////////////////////////////////////////
+   //////////////////////////////////////////////
+  /////////////[ Coded By MrVirus ]///////////////
+ //////////////[ Instagram : BQBB ]////////////////
+///////////////[ Telegram : camera ]/////////////////
+   //////////////////////////////////////////////
+    ///////////////////////////////////////////
+     ////////////////////////////////////////
+''')    
+ip=args()[0]
+li=args()[1]
+if(li):
+    li=open(str(li)+'.txt','r')
+    for i in li:
+        extract(i.strip())
+elif(li==None):
+    if(ip):
+        extract(ip.strip())
+    else:
+        ch=input('[ 1 ] From IP\n[ 2 ] From List\nSelect : ')
+        if(int(ch)==1):
+            ip=input('IP : ')
+            extract(ip.strip())
+        elif(int(ch)==2):
+            li=input('List : ')
+            li=open(li+'.txt','r')
+            for i in li:
+                extract(i.strip())
+        else:
+            exit()
